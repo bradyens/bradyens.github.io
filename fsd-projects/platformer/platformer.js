14190160e7e3b9fa74459bb7b9ece7ce4a705e21
bradyens <braystarbuck09@gmail.com>
2026-09-03 12:21:59 +0000

    the game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,22 +31,44 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
+   createPlatform(125, 370, 25, 275, "black")
+   createPlatform(150, 635, 10, 10, "black")
+   createPlatform(150, 505, 30, 10, "black")
+   createPlatform(400, 350, 5, 200, "black")
+   createPlatform(400, 0, 5, 230, "black")
+   createPlatform(600, 175, 40, 10, "black", 600, 600, 0, 10, 250, 2 )
+   createPlatform(600, 0, 5, 100, "black")
+   createPlatform(640, 0, 5, 230, "black")
+   createPlatform(800, 250, 60, 15, "black")
+   createPlatform(800, 250, 10, 150, "black")
+   createPlatform(800, 400, 60, 15, "black")
+   createBadPlatform(960, 10, 15, 150, "red")
+   createBadPlatform(735, 300, 15, 85, "red")
+   createPlatform(750, 370, 60, 15, "black")
+   createPlatform(1075, 300, 60, 15, "black")
+   createBadPlatform(1200, 250, 10, 30, "red")
+   createBadPlatform(1200, 75, 10, 30, "red")
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("steve", 350, 100)
+    createCollectable("steve", 755, 320)
+    createCollectable("steve", 1300, 225)
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 100, 800);
+    createCannon("right", 650, 200, 150, 150, 700, 500, 20);
+    createCannon("right", 200, 3789, 12, 12, 100, 350, 15)
 
 
     
     
     //////////////////////////////////
-    // ONLY CHANGE ABOVE THIS POINT //
+    // ONLY CHANGE ABOVE THIS POINT // createCannon("right", 750, 100)
     //////////////////////////////////
   }
 
