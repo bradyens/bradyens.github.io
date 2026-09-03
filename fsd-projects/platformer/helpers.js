ee0114fb277f11bea80930bc30636ce57399886a
bradyens <braystarbuck09@gmail.com>
2026-09-03 12:49:24 +0000

    remind pushing platformer changes for you might be updated colors

--- fsd-projects/platformer/helpers.js
+++ fsd-projects/platformer/helpers.js
@@ -398,14 +398,14 @@ function deathOfPlayer() {
   ctx.fillStyle = "black";
   ctx.font = "800% serif";
   ctx.fillText(
-    "You are dead",
+    "u got unalived",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 5,
     (canvas.width / 16) * 14
   );
   ctx.font = "500% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "try winning next time",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
@@ -684,14 +684,14 @@ function winGame() {
   ctx.fillStyle = "white";
   ctx.font = "800% serif";
   ctx.fillText(
-    "You Win!",
+    "how",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 5,
     (canvas.width / 16) * 14
   );
   ctx.font = "500% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "you chaeted",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
